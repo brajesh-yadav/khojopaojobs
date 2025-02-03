f78a74519187af3e2fab6d604d197e76a327135f
brajesh-yadav <87525372+brajesh-yadav@users.noreply.github.com>
2025-02-03 08:18:43 +0530

    Reverted to kv-v2.0

--- about_us.php
+++ about_us.php
@@ -8,7 +8,7 @@
                  <div class="body" style="width: 100%;padding-left:10px;"><h2>Khojopao is a platform to help all who want to be employed or upgrade his/her career.<br>
                 Switching a job means selecting right job that make your career brighter so We tried to provide a platform that can give best choice.<br><br>
             We provide that information through links, pictures or documents.<br><br>
-        Day by day We are upgrading our system  so that you can get better facilities and oppurtunity.For this, We need your help as your comments, suggestions so join with khojopao.com to be smarter.<br><br><h1 style="color:rgb(226, 10, 10)"><center>----Thank You very much----</center><h1></h2> 
+        Day by day We are upgrading our system  so that you can get better facilities and oppurtunity.For this, We need your help as your comments, suggestions so join with khojopao.com to be smarter.<br><br><h1 style="color:rgb(226, 10, 10)"><center>----Thank You----</center><h1></h2> 
         <?php include 'footer.php'?>
               
            
